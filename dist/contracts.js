6951d187977079b1fdacce0cf236906ea45a0ca5
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-06 20:55:30 +0300

    @types output added. dist directory and yarn.lock file are now included in repository. examples added to .npmignore

---
+++ dist/contracts.js
@@ -0,0 +1,8 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+var Verbosity;
+(function (Verbosity) {
+    Verbosity[Verbosity["None"] = 0] = "None";
+    Verbosity[Verbosity["Errors"] = 8] = "Errors";
+    Verbosity[Verbosity["Verbose"] = 256] = "Verbose";
+})(Verbosity = exports.Verbosity || (exports.Verbosity = {}));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udHJhY3RzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbnRyYWN0cy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiO0FBTUEsSUFBWSxTQUlYO0FBSkQsV0FBWSxTQUFTO0lBQ2pCLHlDQUFRLENBQUE7SUFDUiw2Q0FBVSxDQUFBO0lBQ1YsaURBQWEsQ0FBQTtBQUNqQixDQUFDLEVBSlcsU0FBUyxHQUFULGlCQUFTLEtBQVQsaUJBQVMsUUFJcEIiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgaW50ZXJmYWNlIENvbmZpZyB7XHJcbiAgICBFbnRyeTogc3RyaW5nO1xyXG4gICAgRGVzdGluYXRpb246IHN0cmluZztcclxuICAgIFZlcmJvc2l0eTogVmVyYm9zaXR5O1xyXG59XHJcblxyXG5leHBvcnQgZW51bSBWZXJib3NpdHkge1xyXG4gICAgTm9uZSA9IDAsXHJcbiAgICBFcnJvcnMgPSA4LFxyXG4gICAgVmVyYm9zZSA9IDI1NlxyXG59XHJcblxyXG5leHBvcnQgaW50ZXJmYWNlIEFyZ3VtZW50c1ZhbHVlcyB7XHJcbiAgICBjb25maWc6IHN0cmluZztcclxuICAgIGVudHJ5OiBzdHJpbmc7XHJcbiAgICBkZXN0OiBzdHJpbmc7XHJcbiAgICB2ZXJib3NpdHk6IFZlcmJvc2l0eTtcclxufVxyXG4iXX0=
