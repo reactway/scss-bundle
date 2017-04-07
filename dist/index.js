a0305aec4c5a61a090987c0fb0a5859f3241101c
Dovydas Navickas <dovydas@quatrodev.com>
2017-04-07 19:34:56 +0300

    Types added to project.json and index file introduced to export bundler.

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("./bundler"));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLCtCQUEwQiIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCAqIGZyb20gXCIuL2J1bmRsZXJcIjtcclxuIl19
