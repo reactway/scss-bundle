6a31d9a1ba322403fea79930846a949c71a7fd34
Paul Gschwendtner <paulgschwendtner@gmail.com>
2017-04-19 23:19:54 +0200

    build: properly ship typing files
    * Now ships the typing files properly
    * No longer pushes the dist/ and types to the Git repository.
    * Changes all source-files from CRLF to LF

--- dist/contracts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Verbosity;
-(function (Verbosity) {
-    Verbosity[Verbosity["None"] = 0] = "None";
-    Verbosity[Verbosity["Errors"] = 8] = "Errors";
-    Verbosity[Verbosity["Verbose"] = 256] = "Verbose";
-})(Verbosity = exports.Verbosity || (exports.Verbosity = {}));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udHJhY3RzLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vc3JjL2NvbnRyYWN0cy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQU9BLElBQVksU0FJWDtBQUpELFdBQVksU0FBUztJQUNqQix5Q0FBUSxDQUFBO0lBQ1IsNkNBQVUsQ0FBQTtJQUNWLGlEQUFhLENBQUE7QUFDakIsQ0FBQyxFQUpXLFNBQVMsR0FBVCxpQkFBUyxLQUFULGlCQUFTLFFBSXBCIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGludGVyZmFjZSBDb25maWcge1xyXG4gICAgRW50cnk6IHN0cmluZztcclxuICAgIERlc3RpbmF0aW9uOiBzdHJpbmc7XHJcbiAgICBWZXJib3NpdHk6IFZlcmJvc2l0eTtcclxuICAgIERlZHVwZUdsb2JzPzogc3RyaW5nW107XHJcbn1cclxuXHJcbmV4cG9ydCBlbnVtIFZlcmJvc2l0eSB7XHJcbiAgICBOb25lID0gMCxcclxuICAgIEVycm9ycyA9IDgsXHJcbiAgICBWZXJib3NlID0gMjU2XHJcbn1cclxuXHJcbmV4cG9ydCBpbnRlcmZhY2UgQXJndW1lbnRzVmFsdWVzIHtcclxuICAgIGNvbmZpZz86IHN0cmluZztcclxuICAgIGVudHJ5OiBzdHJpbmc7XHJcbiAgICBkZXN0OiBzdHJpbmc7XHJcbiAgICB2ZXJib3NpdHk6IFZlcmJvc2l0eTtcclxuICAgIGRlZHVwZT86IHN0cmluZ1tdO1xyXG59XHJcbiJdfQ==
